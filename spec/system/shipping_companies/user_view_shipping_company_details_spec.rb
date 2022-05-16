10e06d2f29bdbf2758bd4266d99d71166cd2001d
Sergio Gurgel <sergiogurgell@gmail.com>
2022-05-16 19:18:56 -0300

    Usuário vê detalhes de uma transportadora e volta para a lista de transportadoras

--- spec/system/shipping_companies/user_view_shipping_company_details_spec.rb
+++ spec/system/shipping_companies/user_view_shipping_company_details_spec.rb
@@ -24,11 +24,19 @@ describe 'Usuário vê detalhes de uma transportadora' do
     expect(page).to have_content('Rodovia Presidente Dutra, Km 228, Guarulhos - SP')
   end
 
-  # it 'e volta para a lista de transportadoras' do
-  #   # Arrange
+  it 'e volta para a lista de transportadoras' do
+    # Arrange
+    ShippingCompany.create!(corporate_name: 'Fedex Brasil Logistica e Transporte LTDA', brand_name: 'FedEx', registration_number: '10970887000285', email_domain: '@fedex.com.br', address: 'Rodovia Presidente Dutra, Km 228, Guarulhos - SP')
 
-  #   # Act
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Transportadoras'
+    end
+    click_on 'FedEx'
+    click_on 'Voltar'
 
-  #   # Assert
-  # end
+    # Assert
+    expect(current_path).to eq(shipping_companies_path)
+  end
 end
