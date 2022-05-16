2f2b77029039724ba56e19a85a909b528d7916d5
Sergio Gurgel <sergiogurgell@gmail.com>
2022-05-16 15:30:51 -0300

    Testes unitários de uniqueness para Razão Social, CNPJ e Domínio de Email

--- app/models/shipping_company.rb
+++ app/models/shipping_company.rb
@@ -1,3 +1,4 @@
 class ShippingCompany < ApplicationRecord
   validates :corporate_name, :brand_name, :registration_number, :email_domain, :address, presence: true
+  validates :corporate_name, :registration_number, :email_domain, uniqueness: true
 end
--- spec/models/shipping_company_spec.rb
+++ spec/models/shipping_company_spec.rb
@@ -45,6 +45,35 @@ RSpec.describe ShippingCompany, type: :model do
       end
     end
 
+    context 'uniqueness' do
+      it 'falso quando a Razão Social (corporate_name) já estiver em uso' do
+        # Arrange
+        first_sc = ShippingCompany.create!(corporate_name: 'Fedex Brasil Logistica e Transporte LTDA', brand_name: 'FedEx', registration_number: '10970887000285', email_domain: '@fedex.com.br', address: 'Rodovia Presidente Dutra, Km 228, Guarulhos - SP')
+        second_sc = ShippingCompany.new(corporate_name: 'Fedex Brasil Logistica e Transporte LTDA', brand_name: 'Embraen', registration_number: '04512172000103', email_domain: '@embraen.com.br', address: 'Rua Doutor João Marques Mauricio, 278, Embu das Artes - SP')
+  
+        # Act and Assert
+        expect(second_sc).not_to be_valid
+      end
+
+      it 'falso quando o CNPJ (registration_number) já estiver em uso' do
+        # Arrange
+        first_sc = ShippingCompany.create!(corporate_name: 'Fedex Brasil Logistica e Transporte LTDA', brand_name: 'FedEx', registration_number: '10010010010010', email_domain: '@fedex.com.br', address: 'Rodovia Presidente Dutra, Km 228, Guarulhos - SP')
+        second_sc = ShippingCompany.new(corporate_name: 'Embraen Empresa Brasileira de Transportes Eireli', brand_name: 'Embraen', registration_number: '10010010010010', email_domain: '@embraen.com.br', address: 'Rua Doutor João Marques Mauricio, 278, Embu das Artes - SP')
+  
+        # Act and Assert
+        expect(second_sc).not_to be_valid
+      end
+
+      it 'falso quando o Domínio de Email (email_domain) já estiver em uso' do
+        # Arrange
+        first_sc = ShippingCompany.create!(corporate_name: 'Fedex Brasil Logistica e Transporte LTDA', brand_name: 'FedEx', registration_number: '10970887000285', email_domain: '@fedex.com.br', address: 'Rodovia Presidente Dutra, Km 228, Guarulhos - SP')
+        second_sc = ShippingCompany.new(corporate_name: 'Embraen Empresa Brasileira de Transportes Eireli', brand_name: 'Embraen', registration_number: '04512172000103', email_domain: '@fedex.com.br', address: 'Rua Doutor João Marques Mauricio, 278, Embu das Artes - SP')
+  
+        # Act and Assert
+        expect(second_sc).not_to be_valid
+      end
+    end
+
   end
 
 end
