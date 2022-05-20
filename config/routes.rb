f8913f412082f1c93e1e86bc0765921afea98293
Sergio Gurgel <sergiogurgell@gmail.com>
2022-05-19 23:21:00 -0300

    Configurações de 2 (dois) models devise (Admin e User)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  devise_for :admins, path: 'admins', controllers: { sessions: 'admins/sessions', registrations: 'admins/registrations' }
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  root to: 'home#index'
   resources :shipping_companies, only: [:index, :show, :new, :create, :edit, :update]
 end
---
+++ spec/system/admins/admin_sign_in_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe 'Usuário de Administrador se autentica (faz login)' do
+  it 'na página de login de Admin' do
+    # Arrange
+    Admin.create!(email: 'teste@sistemadefrete.com.br', password: 'admin123')
+
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Fazer Login como Admin'
+    end
+
+    # Assert
+    expect(current_path).to eq(new_admin_session_path)
+  end
+
+  it 'com sucesso' do
+    # Arrange
+    Admin.create!(email: 'teste@sistemadefrete.com.br', password: 'admin123')
+
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Fazer Login como Admin'
+    end
+    fill_in 'E-mail', with: 'teste@sistemadefrete.com.br'
+    fill_in 'Senha', with: 'admin123'
+    click_on 'Entrar'
+
+    # Assert
+    expect(page).to have_content('Login Admin efetuado com sucesso.')
+    expect(page).not_to have_link('Fazer Login como Admin')
+    expect(page).to have_link('Sair')
+    expect(page).to have_content('teste@sistemadefrete.com.br')
+  end
+
+  it 'e faz logout' do
+    # Arrange
+    Admin.create!(email: 'teste@sistemadefrete.com.br', password: 'admin123')
+
+    # Act
+    visit root_path
+    within('nav') do
+      click_on 'Fazer Login como Admin'
+    end
+    fill_in 'E-mail', with: 'teste@sistemadefrete.com.br'
+    fill_in 'Senha', with: 'admin123'
+    click_on 'Entrar'
+    click_on 'Sair'
+
+    # Assert
+    expect(page).to have_content('Admin Saiu')
+    expect(page).to have_link('Entrar')
+    expect(page).not_to have_link('Sair')
+    expext(page).not_to have_content('teste@sistemadefrete.com.br')
+  end
+  
+end
